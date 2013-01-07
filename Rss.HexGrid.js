0688a3e5973f6481639b59a975a6e2a89f19e445
sebastian <sebastian@sebastian-desktop>
2013-01-07 17:07:44 +0200

    hex grid fun

--- Rss.HexGrid.js
+++ Rss.HexGrid.js
@@ -1,17 +1,17 @@
 'use strict';
 /*
- * requires Rss.Grid
+ * requires Rss.HexGrid
  */
-Rss.Grid = function(x, y, squareSize, hasBorder, borderColor, appendTo, canvasId) {
+Rss.Grid = function(x, y, hexagonSize, hasBorder, borderColor, appendTo, canvasId) {
     this.x = x;
     this.y = y;
-    this.squareSize = squareSize;
+    this.squareSize = hexagonSize;
     this.borderSize = hasBorder ? 1 : 0;
     
     var rssCanvas = new Rss.Canvas(appendTo, canvasId);
     
-    var canvasWidth     = x * squareSize + this.borderSize;
-    var canvasHeight    = y * squareSize + this.borderSize;
+    var canvasWidth     = x * hexagonSize + this.borderSize;
+    var canvasHeight    = y * hexagonSize + this.borderSize;
 
     rssCanvas.getCanvas().setAttribute('width', canvasWidth);
     rssCanvas.getCanvas().setAttribute('height', canvasHeight);
@@ -19,12 +19,14 @@ Rss.Grid = function(x, y, squareSize, hasBorder, borderColor, appendTo, canvasId
     var context = rssCanvas.getContext2d();
     
     var _makeGrid = function() {
-        for (var x = 0; x <= canvasWidth; x += squareSize) {
-            context.moveTo(x + 0.5, 0);
-            context.lineTo(x + 0.5, canvasHeight);
+        for (var x = 0; x <= canvasWidth; x += hexagonSize) {
+            for (var y=hexagonSize; y<= canvasHeight; y += hexagonSize) {
+                context.moveTo(x + 0.5, y * 1 / 4);
+                context.lineTo(x + 0.5, y * 3 / 4);
+            }
         }
 
-        for (var x = 1; x <= canvasHeight; x += squareSize) {
+        for (var x = 1; x <= canvasHeight; x += hexagonSize) {
             context.moveTo(0, x - 0.5);
             context.lineTo(canvasWidth, x - 0.5);
         }
